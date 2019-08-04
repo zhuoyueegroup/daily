6df143eddf594db7af008447e18050237f85a0b3
yokong <657715602@qq.com>
2019-08-04 17:26:10 +0800

    test

--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <van-tabbar route v-model="active">
+  <div class="tabbar-container">
+    <van-tabbar route v-model="active" border class="tabbar">
       <van-tabbar-item name="home" :to="{name:'home'}" icon="home-o">主页</van-tabbar-item>
-      <van-tabbar-item name="classify" :to="{name:'classify'}"   icon="search">分类</van-tabbar-item>
-      <van-tabbar-item name="seckill" :to="{name:'seckill'}"  icon="friends-o">秒杀</van-tabbar-item>
-      <van-tabbar-item name="shopping" :to="{name:'shopping'}"  icon="friends-o">购物车</van-tabbar-item>
-      <van-tabbar-item name="person" :to="{name:'person'}"  icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item name="classify" :to="{name:'classify'}"   icon="more-o">分类</van-tabbar-item>
+      <van-tabbar-item name="seckill" :to="{name:'seckill'}"  icon="hot-o">秒杀</van-tabbar-item>
+      <van-tabbar-item name="shopping" :to="{name:'shopping'}"  icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item name="person" :to="{name:'person'}"  icon="manager-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -19,3 +19,13 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.tabbar-container{
+  .tabbar{
+    height: 100px;
+    background-color: pink;
+  }
+}
+</style>
+
