9b2a72b13751d291407e0a7e3b3b9d51b84aaf3e
liangyv <1531197646@qq.com>
2019-08-08 20:30:04 +0800

    mm

--- src/main.js
+++ src/main.js
@@ -10,14 +10,10 @@ import {store} from './store/store'
 import axios from 'axios'
 
 // 按需加载vant组件
-<<<<<<< HEAD
-import { Button,Loading,Tabbar, TabbarItem ,Sidebar, SidebarItem ,Search,Icon,Panel,Image,Field,Cell, CellGroup,Card,Tag,Tab, Tabs,CountDown,Checkbox,CheckboxGroup,NavBar, ContactCard, ContactList, ContactEdit, Popup ,RadioGroup, Radio, CouponCell, CouponList } from 'vant';
-Vue.use(Button).use(Loading).use(Tabbar).use(TabbarItem).use(Sidebar).use(SidebarItem).use(Search).use(Icon).use(Panel).use(Image).use(Field).use(Cell).use(CellGroup).use(Card).use(Tag).use(Tab).use(Tabs).use(CountDown).use(Checkbox).use(CheckboxGroup).use(CouponCell).use(CouponList)
+import { Button,Loading,Tabbar, TabbarItem ,Sidebar, SidebarItem ,Search,Icon,Panel,Image,Field,Cell, CellGroup,Card,Tag,Tab, Tabs,CountDown,Checkbox,CheckboxGroup,NavBar, ContactCard, ContactList, ContactEdit, Popup ,RadioGroup, Radio, CouponCell, CouponList,Grid,GridItem } from 'vant';
+Vue.use(Button).use(Loading).use(Tabbar).use(TabbarItem).use(Sidebar).use(SidebarItem).use(Search).use(Icon).use(Panel).use(Image).use(Field).use(Cell).use(CellGroup).use(Card).use(Tag).use(Tab).use(Tabs).use(CountDown).use(Checkbox).use(CheckboxGroup).use(CouponCell).use(CouponList).use(Grid).use(GridItem )
 .use(NavBar).use(ContactCard).use(ContactList).use(ContactEdit).use(Popup).use(RadioGroup).use(Radio);
-=======
-import { Button,Loading,Tabbar, TabbarItem ,Sidebar, SidebarItem ,Search,Icon,Panel,Image,Field,Cell, CellGroup,Card,Tag,Tab, Tabs,CountDown,Checkbox,CheckboxGroup,Grid,GridItem } from 'vant';
-Vue.use(Button).use(Loading).use(Tabbar).use(TabbarItem).use(Sidebar).use(SidebarItem).use(Search).use(Icon).use(Panel).use(Image).use(Field).use(Cell).use(CellGroup).use(Card).use(Tag).use(Tab).use(Tabs).use(CountDown).use(Checkbox).use(CheckboxGroup).use(Grid).use(GridItem );
->>>>>>> 7a364a7d33b701f5ac46d520484275b10c708cf2
+
 
 Vue.config.productionTip = false
 /*rnrnrn*/
